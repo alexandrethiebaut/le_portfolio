ba929fdf63bc6fb306786f4a0a0d9642e35db2d5
Alexandre Thiebaut <thiebaut.alex@gmail.com>
2015-06-05 11:45:41 +0200

    Adaptation responsive "menu overlay"

--- parts/menu-overlay.php
+++ parts/menu-overlay.php
@@ -2,6 +2,8 @@
 	<div id="main-menu">
 		<img src="<?php echo( get_header_image() ); ?>" alt="<?php echo( get_bloginfo( 'title' ) ); ?>" />
 	    <?php foundationpress_mobile_off_canvas(); ?>
-	    <?php menu_lang(); ?>
+	    <div id="menu-lang">
+			<?php menu_lang(); ?>
+		</div>
     </div>
 </aside>

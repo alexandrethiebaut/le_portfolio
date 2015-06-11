6a3b347589dffe69e240417671e7ea63e47e3609
Alexandre Thiebaut <thiebaut.alex@gmail.com>
2015-06-11 11:17:29 +0200

    Update du style "Homepage"

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	
 	<div class="small-offset-1 small-10 medium-offset-1 medium-6 columns padding-page">
 
-		<div>
+		<div class="this-is-homepage">
 			<?php echo $post_desc->post_content; ?>
 		</div>
 
@@ -25,43 +25,3 @@
 
 </div>
 <?php get_footer(); ?>
-
-<!-- 	<?php get_sidebar(); ?>
-	<div class="small-12 large-8 columns" role="main">
-
-		<div class="row collapse">
-			
-			<ul class="small-block-grid-1 medium-block-grid-2">
-			<?php if ( have_posts() ) : ?>
-				
-				<?php query_posts(array('cat' => CAT_PROJETS)); ?>
-
-				<?php do_action( 'foundationpress_before_content' ); ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'content-home', get_post_format() ); ?>
-				<?php endwhile; ?>
-
-				<?php else : ?>
-					<?php get_template_part( 'content', 'none' ); ?>
-
-				<?php do_action( 'foundationpress_before_pagination' ); ?>
-
-				<?php wp_reset_query(); ?>
-
-			<?php endif;?>
-			</ul>
-
-		</div>
-
-	<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-		</nav>
-	<?php } ?>
-
-	<?php do_action( 'foundationpress_after_content' ); ?>
-
-	</div>
- -->
